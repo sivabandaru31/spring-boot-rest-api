a4daec7fd795bec56e67a19c2d31fd65f2624d62
sivabandaru31 <siva.bandaru31@gmail.com>
2026-09-18 22:29:40 +0530

    Add @request param

--- src/main/java/com/javaguids/spring_boot_rest_api/controller/StuudentController.java
+++ src/main/java/com/javaguids/spring_boot_rest_api/controller/StuudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import com.javaguids.spring_boot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 
@@ -36,5 +37,14 @@ public class StuudentController {
     @GetMapping("students/{id}/{firstname}/{lastname}")
     public Student studentPathVarisble(@PathVariable int id,@PathVariable String firstname,@PathVariable String lastname){
         return new Student(id,firstname,lastname);
+    }
+
+    //spring boot rest api with Request param
+    //http://localhost:8081/students/query?id=1&firstName=sivakrishna&lastName=bandaru
+    @GetMapping("students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam  String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id,firstName,lastName);
     }
 }
