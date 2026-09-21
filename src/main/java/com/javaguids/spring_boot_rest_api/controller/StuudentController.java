28ff12b8670f79ea9082065abb13ba8d644a53bf
sivabandaru31 <siva.bandaru31@gmail.com>
2026-09-21 20:43:33 +0530

    HTTP PUT Request-updating existing resourse

--- src/main/java/com/javaguids/spring_boot_rest_api/controller/StuudentController.java
+++ src/main/java/com/javaguids/spring_boot_rest_api/controller/StuudentController.java
@@ -2,10 +2,9 @@ package com.javaguids.spring_boot_rest_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 import com.javaguids.spring_boot_rest_api.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 
 public class StuudentController {
@@ -46,5 +45,25 @@ public class StuudentController {
                                           @RequestParam  String firstName,
                                           @RequestParam String lastName){
         return new Student(id,firstName,lastName);
+    }
+
+    //SPRING BOOT REST API THAT HANDLE HTTP POST REQUEST-to creating new resource
+    //@PostMapping && @RequestBody
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstname());
+        System.out.println(student.getLastname());
+        return student ;
+    }
+
+    //Spring boot REST API thst handles HTTP PUT Request-updating existing resourse
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student students,@PathVariable("id") int studentId){
+        System.out.println(students.getFirstname());
+        System.out.println(students.getLastname());
+        return students;
     }
 }
